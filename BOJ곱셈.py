7f6645ebe6a934811a9d4b0ae944f790bc650cb1
정우재 <80890472+woojae05@users.noreply.github.com>
2022-01-29 17:15:07 +0900

    study: algorithm
    BOJ 입출력과 사칙연산 단계

---
+++ BOJ곱셈.py
@@ -0,0 +1,5 @@
+n = int(input())
+m = input()
+for i in reversed(m):
+    print(n*int(i))
+print(n*int(m))
---
+++ BOJ사칙연산.py
@@ -0,0 +1,7 @@
+import math
+A, B = map(int, input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(math.trunc(A/B))
+print(A % B)
