a68a64f05738fba7867f958dd6ff86419a9ed614
정우재 <80890472+woojae05@users.noreply.github.com>
2022-03-02 13:43:02 +0900

    Update BOJ그룹 단어 체커.py

--- BOJ그룹 단어 체커.py
+++ BOJ그룹 단어 체커.py
@@ -1,10 +1,12 @@
 n = int(input())
-cnt = 0
+cnt = n
+
 for i in range(n):
-    S = input()
-    list = []
-    for j in range(len(S)):
-        for k in S:
-            if S[j] != k:
-                list.append(k)
-print(list)
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
