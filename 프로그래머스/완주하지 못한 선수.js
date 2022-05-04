9ad136309bab769664f81abbdd05e1757fc69d50
정우재 <80890472+woojae05@users.noreply.github.com>
2022-05-04 22:49:32 +0900

    Update 완주하지 못한 선수.js

--- 프로그래머스/완주하지 못한 선수.js
+++ 프로그래머스/완주하지 못한 선수.js
@@ -2,16 +2,13 @@ const participant1 = ["mislav", "stanko", "mislav", "ana"];
 const completion1 = ["stanko", "ana", "mislav"];
 
 function solution(participant, completion) {
-  let answer;
-  participant = participant.sort();
-  completion = completion.sort();
-  let str = (participant = participant.join(""));
-  for (let i = 0; i < completion.lenght; i++) {
-    str = str.replace(completion[i], "");
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    }
   }
-  console.log(str);
-
-  return answer;
 }
-
 console.log(solution(participant1, completion1));
