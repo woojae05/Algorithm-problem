6c442a74ce9ee60af445b419d7f02bbcf78b36e4
정우재 <80890472+woojae05@users.noreply.github.com>
2022-05-08 23:16:16 +0900

    Create 체육복.js

---
+++ 프로그래머스/체육복.js
@@ -0,0 +1,31 @@
+const n1 = 5;
+const lost1 = [4, 3];
+const reserve1 = [3, 2];
+
+function solution(n, lost, reserve) {
+  const students = {};
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    students[i] = 1;
+  }
+  lost.forEach((number) => (students[number] -= 1));
+  reserve.forEach((number) => (students[number] += 1));
+
+  for (let i = 1; i <= n; i++) {
+    if (students[i] === 2 && students[i - 1] === 0) {
+      students[i - 1]++;
+      students[i]--;
+    } else if (students[i] === 2 && students[i + 1] === 0) {
+      students[i + 1]++;
+      students[i]--;
+    }
+  }
+  for (let key in students) {
+    if (students[key] >= 1) {
+      answer++;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(n1, lost1, reserve1));
